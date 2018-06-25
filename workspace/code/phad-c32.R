b9beab4759a880141236d2c6b1d2e73ba0cec0f2
nuctools <javiermb.desarrollo@gmail.com>
2018-06-25 07:55:06 +0200

    Updated scoring and labeled functions

--- workspace/code/phad-c32.R
+++ workspace/code/phad-c32.R
@@ -7,12 +7,8 @@ source(paste(root.dir,"cache/cacheLoader.R",sep="/"))
 source(paste(root.dir,"input/inputLoader.R",sep="/"))
 source(paste(root.dir,"util/clusterer.R",sep="/"))
 
-logger <- function(message = "None", time=TRUE) {
-  if( time ) {
-    cat(paste("[",Sys.time(),"] ", message,"\n",sep=""))
-  } else {
-    cat(paste(message,"\n",sep=""))
-  }
+logger <- function(message) {
+  print(paste("[",Sys.time(),"] ", message))
 }
 
 #' @param cache     Do not do any calculation, use precalculated data
@@ -128,23 +124,42 @@ labelTestingDS <- function(testing.raw.ds, label.attacklist.raw.ds, cache = FALS
     
   }
   
-  if( is.null(cache)) {
+  if( !cache || is.null(label.testing.raw.ds)) {
     
-    label.testing.raw.ds <- testing.raw.ds %>% select(timestamp,ip.dst) %>% mutate( ts = trunc(timestamp / 1000000),  attack = FALSE)
-    
+    #label.testing.raw.ds <- testing.raw.ds %>% select(timestamp,ip.dst) %>% mutate( ts = trunc(timestamp / 1000000),  attack = FALSE)
+    label.testing.raw.ds <- testing.raw.ds %>% select(timestamp,ip.dst) %>% mutate( ts = timestamp,  attack = FALSE, name = NA)
     
     tic()
-    for(index in 1:nrow(label.attacklist.raw.ds)){
+    nrows <- nrow(label.attacklist.raw.ds)
+    for(index in 1:nrows){
       
       
-      logger( paste("[",index, "/",nrow(label.attacklist.raw.ds),"] Processing attack..." ))
+      logger( paste("[",index, "/",nrows, "] Processing attack..." ))
       
       tsStart <-label.attacklist.raw.ds$starttime[index]
       tsEnd <- label.attacklist.raw.ds$endtime[index]
       ipDst <- label.attacklist.raw.ds$dstIP[index]
+      name <- label.attacklist.raw.ds$name[index]
       
       label.testing.raw.ds <- label.testing.raw.ds %>% 
-        mutate( attack = attack %>% replace( attack == FALSE & ts >= tsStart & ts <= tsEnd & ip.dst == ipDst, TRUE))
+        mutate( attack = attack %>% replace( attack == FALSE & ts >= tsStart & ts <= tsEnd & ip.dst == ipDst, TRUE) )
+      
+    }
+    
+    nrows <- nrow( label.testing.raw.ds)
+    for(index in 1:nrows){
+      
+      logger( paste("[",index, "/",nrows,"] Getting name attack..." ))
+      
+      if( isTRUE(label.testing.raw.ds[['attack']][index])) {
+        
+        ts <- label.testing.raw.ds[['ts']][index] 
+        ipDst <- label.testing.raw.ds[['ip.dst']][index] 
+        
+        attackName <- first(label.attacklist.raw.ds %>% filter( starttime <= ts & endtime >= ts & dstIP == ipDst ) %>% select (name))
+        label.testing.raw.ds[['name']][index] <- attackName
+        
+      }
       
     }
     
@@ -254,9 +269,11 @@ scoring <- function(model.ds,testing.raw.ds, label.testing.raw.ds, cache = TRUE,
       
       if( ! exists("score") || is.null(score)  ) {
         
+
         score <- scoreFeature(model.ds,testing.raw.ds[indexFeatureOnTraining], 
                               indexFeatureOnModel, indexFeatureOnTraining, indexTimestampFeature,
                               lastAnomaly[1, indexFeatureOnModel], nr[indexFeatureOnModel], testing.raw.ds[indexTimestampFeature])
+
         # Save score
         saveCacheScoringFeature(score, indexFeatureOnModel, root.dir)
       }
@@ -329,12 +346,6 @@ scoreFeature <- function(model.ds,testing.raw.ds, indexFeatureOnModel, indexFeat
     if(! is.na(valueFeature ) ) { 
       
       if( model.ds[[indexFeatureOnModel]]$contains(valueFeature) ) {
-        
-        # str(lastAnomaly[feature] -> lastAnomaly
-        # timestamp -> initialTimestamp
-        timestamp<-testing.raw.ds.timestamp[[1]][indexObs]
-        logger(paste("ANALISIS","|",indexFeatureOnModel,"|",timestamp,"|",lastAnomaly,"|","NA","|","NA","|","NA","|","NA","|",valueFeature,"|","INCLUDED",sep=""),FALSE)
-        
         # Do nothing. It is a valid value for that feature
         numNoAnomalies <- numNoAnomalies + 1
       } else {
@@ -351,12 +362,10 @@ scoreFeature <- function(model.ds,testing.raw.ds, indexFeatureOnModel, indexFeat
         # Update current timestamp
         lastAnomaly = timestamp
         
-        logger(paste("ANALISIS","|",indexFeatureOnModel,"|",timestamp,"|",lastAnomaly,"|",t,"|","NA","|","|",scores[indexObs],"|",valueFeature,"|","CALCULATED", sep=""),FALSE)
-    
       }
       
     } else {
-      logger(paste("ANALISIS","|",indexFeatureOnModel,"|",initialTimestamp,"|",lastAnomaly,"|","NA","|","NA","|","NA","|","NA",valueFeature,"|","INVALID",sep=""),FALSE)
+      
       numNA <- numNA +1
       
     }
@@ -411,5 +420,211 @@ getModelDataframe <- function(model.ds)
   return(training.tabular.model.ds)
   
   
+}
+
+#' @param model.ds             Model used to decide whether a register is an attack or not 
+#' @param label.testing.raw.ds Optional. Labeled attacks on testing dataset
+#' @param testing.raw.ds       Data set for evaluation. Decide if some packet is an attack or not
+#' @param cache                [TODO] Do not do any calculation, use precalculated data
+#' @param root.dir             [TODO] Workspace root directory used to load cached data
+scoringAggregate <- function(model.ds,testing.raw.ds, label.testing.raw.ds, cache = TRUE, root.dir) {
+  
+  logger("init build")
+  
+  # Just for information
+  if( ! is.na(Sys.getenv()["worker_all"]) & !  is.na(Sys.getenv()["worker_current"])) {
+    worker.all <- as.numeric(Sys.getenv("worker_all"))
+    worker.current <- as.numeric(Sys.getenv("worker_current"))  
+  }
+  
+  tic(paste("Iniciando cálculo de scoring agregado en worker ", worker.current ))
+  
+  allFeaturesNames <- getAllFeatureNames() 
+  
+  # Constants depending on columns of the dataset
+  offsetFeatures <- 6
+  timestampCol <- 6
+  
+  featureNames <- allFeaturesNames[(offsetFeatures+1):length(allFeaturesNames)]
+  nfeatures <- length(featureNames)
+  
+  scoreNFeatures <- nfeatures
+  scoreNRows <- nrow(testing.raw.ds)
+  
+  # Step 1)
+  # Initialize last anomaly time to 1 sec before time of first packet.
+  # The first column of testData is the timestamp.
+  # TODO: lastAnomaly = {key: testData[0][0] - 1 for key in FEATURES}
+  # TODO: Set initial timestamp for each feature according to most recent pacakge.
+  initialTs <- testing.raw.ds[1,timestampCol] -1
+  lastAnomaly <- data.frame(matrix(rep(initialTs,scoreNFeatures), nrow=1, ncol = scoreNFeatures))
+  names(lastAnomaly) <- featureNames
+  
+  
+  # Step 2)
+  ## TODO: nr = {key: (clusters[key].getTotal(), clusters[key].getDistinct()) for key in FEATURES}
+  # TODO: Repasar si es correcto
+  nr <- rep(0,nfeatures)
+  for(index in 1:nfeatures) {
+    # Note: model.ds.cahce can be accesses as associative array: ie. model.ds.cache$eth.size, instead of using numerical index
+    nr[index] <- model.ds[[index]]$getTotal()/model.ds[[index]]$getDistinct() 
+  }
+  
+  # Step 3)
+  # Create a score dataset only with features to evaluate. Information related to week, day, tstamp are not inside this dataset
+  scoresAll <- data.frame(matrix(rep(0,scoreNFeatures*scoreNRows,nfeatures),nrow = scoreNRows, ncol=scoreNFeatures))
+  names(scoresAll) <- featureNames
+  
+  # Step 4) Calculate scoring
+  # for(indexFeature in testFeature:scoreNFeatures) {
+  for(indexFeature in 1:scoreNFeatures) {
+    
+    # Initialize information for the process
+    indexFeatureOnModel <- indexFeature
+    indexFeatureOnTraining <- indexFeature + offsetFeatures
+    indexTimestampFeature  <- timestampCol
+    
+    # Load score
+    scoresAll[,indexFeature] <- loadCacheScoringFeature(indexFeatureOnModel, root.dir) 
+    
+  }
+  
+  
+  # Rebuild score matrix
+  
+  # Accumulate scoring
+  # Score the packet and store as last element
+  # scores[:, -1] = np.sum(scores[:, 0:-1], axis=1)
+  #### ACTIVATE 
+  scoresAll2 <- scoresAll %>% mutate(totalScore = rowSums(.[1:33]))
+  
+  # If the total score of the packet is very small, set it to one so
+  # that the resulting normalization doesn't fail.
+  # scores[:, -1][scores[:, -1] < 1] = 1
+  #### ACTIVATE scores %>% mutate(totalScore = ifelse(totalScore < 1, 1, totalScore))
+  
+  #results = np.hstack((testData, scores))
+  #np.save(open("data/phad_results.npy", "wb"), results)
+  
+  scoresAll2 <- scoresAll2 %>% select(totalScore)
+  
+  logger("end build")
+  
+  return(scoresAll2)
+  
+}
+
+# 'This functions is only for results validation. Create a dataframe with the following information from model
+# ' Fieldname: feature
+# ' r/n: text containrin r value + character "/" + n Value
+# ' r: value of r
+# ' n: value of n
+# ' Values: first two values
+#' @param model.ds             Model used to decide whether a register is an attack or not 
+getModelDataframe <- function(model.ds) 
+{
+  
+  featureNames <-  c("Field Name","r/n","r","n","Values");
+  
+  nrow <-  length(model.ds)
+  ncol <- length(featureNames)
+  
+  
+  training.tabular.model.ds <- data.frame(matrix(rep(0,nrow,ncol),nrow=nrow,ncol=ncol))
+  trainingFeatureNames <- names(model.ds)
+  
+  
+  
+  for(nFeature in 1:nrow) {
+    
+    logger(nFeature)
+    
+    clusterFeature <- model.ds[[nFeature]];
+    
+    training.tabular.model.ds[nFeature,1] <-  trainingFeatureNames[nFeature]
+    training.tabular.model.ds[nFeature,3] <-  clusterFeature$getDistinct()
+    training.tabular.model.ds[nFeature,4] <-  clusterFeature$getTotal()
+    training.tabular.model.ds[nFeature,2] <-  paste(training.tabular.model.ds[nFeature,3],'/',training.tabular.model.ds[nFeature,4])
+    training.tabular.model.ds[nFeature,5] <-  paste(
+      paste(clusterFeature$getClusters()[[2]][1],'-',clusterFeature$getClusters()[[3]][1],sep=''), 
+      paste(clusterFeature$getClusters()[[2]][2],'-',clusterFeature$getClusters()[[3]][2],sep=''),
+      paste(clusterFeature$getClusters()[[2]][3],'-',clusterFeature$getClusters()[[3]][3],sep=''),
+      sep=' ')
+  }
+  
+  names(training.tabular.model.ds) <- featureNames;
+  
+  return(training.tabular.model.ds)
+  
+  
+}
+
+
+getLabeledTesgingData <- function(testing.raw.ds, label.testing.raw.ds, cache = TRUE, root.dir) {
+  
+  testing.raw.ds <- bind_cols(testing.raw.ds, label.testing.raw.ds['attack'])
+  testing.raw.ds <- bind_cols(testing.raw.ds, label.testing.raw.ds['name'])
+  
+  return(testing.raw.ds)
+  
+}
+
+
+
+#' Temporary function to load off scores from cache
+getScoredTestingData <- function(model.ds, testing.raw.ds, cache = TRUE, root.dir) {
+  
+  logger("begin score build")
+  
+  allFeaturesNames <- getAllFeatureNames() 
+  
+  # Constants depending on columns of the dataset
+  offsetFeatures <- 6
+  timestampCol <- 6
+  
+  featureNames <- allFeaturesNames[(offsetFeatures+1):length(allFeaturesNames)]
+  nfeatures <- length(featureNames)
+  
+  scoreNFeatures <- nfeatures
+  scoreNRows <- nrow(testing.raw.ds)
+  
+  # Create a score dataset only with features to evaluate. Information related to week, day, tstamp are not inside this dataset
+  scoresAll <- data.frame(matrix(rep(0,scoreNFeatures*scoreNRows,nfeatures),nrow = scoreNRows, ncol=scoreNFeatures))
+  names(scoresAll) <- featureNames
+  
+  # Step 4) Calculate scoring
+  # for(indexFeature in testFeature:scoreNFeatures) {
+  for(indexFeature in 1:scoreNFeatures) {
+    
+    # Initialize information for the process
+    indexFeatureOnModel <- indexFeature
+    
+    # Load score
+    scoresAll[,indexFeature] <- loadCacheScoringFeature(indexFeatureOnModel, root.dir) 
+    
+  }
+  
+  # Add an accumulate column
+  scoresAll <- scoresAll %>% mutate(scoring = rowSums(.[1:33]))
+  
+  # Add accumulated score to training dataset
+  testing.raw.scored.ds <- bind_cols(testing.raw.ds, scoresAll['scoring'])
+  
+  # Normalizce scoring
+  testing.raw.scored.ds <- testing.raw.scored.ds %>% mutate ( scoringNorm = lapply( testing.raw.scored.ds['scoring'], minMaxScaler )[[1]])
+  
+  logger("end score build")
+  
+  return(testing.raw.scored.ds)
+  
+}
+
+#' Do MinMax scaler/normalizacion
+# x: data. numeric vector of values to be scaled
+# ' minVal: minimum value
+# ' maxVal: maximum value
+# based on: https://gist.github.com/swayson/b5a6d3cd796ab1d08df1
+minMaxScaler <- function(x, minVal=1, maxVal=100) {
+  return ( (((maxVal-minVal)*(x - min(x))) / (max(x) - min(x))) + minVal )
 }
 
