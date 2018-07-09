2cc46724145c86199a642d962d02716719bb90f5
nuctools <javiermb.desarrollo@gmail.com>
2018-07-09 22:59:28 +0200

    Deprecated code cleaned for input loader

--- workspace/code/input/inputLoader.R
+++ workspace/code/input/inputLoader.R
@@ -4,17 +4,8 @@ source(paste(root.dir,"cache/cacheLoader.R",sep="/"))
 
 PATTERN_TRAINING <- ".*training.*"
 PATTERN_TESTING <- ".*testing.*"
-#PATTERN_TESTING <- "1999_testing_week4_monday_inside.csv"
-PATTERN_ATTACK_LIST <- "master-listfile-condensed.csv"
-PATTERN_ATTACK_TYPE_LIST <- "master-attack-types.csv"
-INPUT_RAW_DIR <- "raw"
-
-
-getRawDir <- function(root.dir = getwd()) {
-  
-  return( paste(root.dir,"..","raw",sep="/"))
-  
-}
+PATTERN_ATTACK_LIST <- "master-listfile-condensed.csv$"
+PATTERN_ATTACK_TYPE_LIST <- "master-attack-types.csv$"
 
 #' Load training dataset
 loadTrainingDataset <- function(cache = FALSE, root.dir=getwd()) {
@@ -104,9 +95,10 @@ loadLabelAttackList <- function(cache = FALSE, root.dir=getwd()) {
     
     #  Path containing dataset input files
     input.dir <- getRawDir(root.dir)
-    
+   
     for (file in list.files(input.dir, pattern = PATTERN_ATTACK_LIST)) {
-      
+      print(input.dir)
+      print(file)
       label.attacklist.raw.ds <- read.csv(paste(input.dir,file,sep="/"),header=FALSE,sep="|",stringsAsFactors = FALSE,
                                           colClasses=c("character","double","double","double","character","character")
       )
@@ -126,6 +118,46 @@ loadLabelAttackList <- function(cache = FALSE, root.dir=getwd()) {
   
 }
 
+#' Load relation between their name and their type or category
+#' 
+#' @description Load relation between their name and their type or category. It is only one file, a CSV file with "|" separator. The file format is:
+#'
+#'   Column01: name		        Name of the attack
+#'   Column02: type   	      Category of the attack
+#'
+#' @param cache     Do not do any calculation, use precalculated data
+#' @param root.dir  Workspace root directory used to load cached data
+loadLabelAttackTypes <- function(cache = FALSE, root.dir=getwd()) {
+  
+  if( cache ) {
+    
+    label.attacktypes.raw.ds <- loadCacheLabelAttackTypesRaw(root.dir) 
+    
+  } else {
+    
+    #  Path containing dataset input files
+    input.dir <- getRawDir(root.dir)
+    
+    for (file in list.files(input.dir, pattern = PATTERN_ATTACK_TYPE_LIST)) {
+      
+      label.attacktypes.raw.ds <- read.csv(paste(input.dir,file,sep="/"),header=FALSE,sep="|",stringsAsFactors = FALSE,
+                                          colClasses=c("character", "character")
+      )
+      
+    }
+    
+    if( ! exists("label.attacktypes.raw.ds") ) {
+      print(paste("WARNING: No label attack type files found, creating an empty vector (input directory[",input.dir,"]"))
+      label.attacklist.raw.ds <- c("0","0")
+    }
+    
+    names(label.attacktypes.raw.ds) <- c("name","type");
+    
+  }  
+  
+  return(label.attacktypes.raw.ds)
+  
+}
 
 #' Load dataset from all files included in the input.dir path
 #' 
@@ -181,17 +213,17 @@ loadDataset <- function(input.dir, pattern = NULL, filelist = NULL) {
   if( is.null(filelist ) ) {
     filelist = list.files(input.dir, pattern = pattern)
   }
-
+  
   for (file in filelist ) {  
-  
-      tic()
     
-      print(paste("Cargando archivo [",file,"]"))
-  
-      dataset.entries <- read.csv(paste(input.dir,file,sep="/"),header=FALSE,sep="|",stringsAsFactors = FALSE,
+    tic()
+    
+    print(paste("Cargando archivo [",file,"]"))
+    
+    dataset.entries <- read.csv(paste(input.dir,file,sep="/"),header=FALSE,sep="|",stringsAsFactors = FALSE,
                                 colClasses=c("character","character","character","character","character","double","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
     )
-     
+    
     if (exists('all.dataset.entries')) {
       all.dataset.entries <- dplyr::bind_rows(all.dataset.entries,dataset.entries)
     } else {
@@ -200,7 +232,7 @@ loadDataset <- function(input.dir, pattern = NULL, filelist = NULL) {
     
     rnorm(1000,0,1)
     toc()
-  
+    
   }
   
   if( !exists( "all.dataset.entries" ) ) {
@@ -212,140 +244,3 @@ loadDataset <- function(input.dir, pattern = NULL, filelist = NULL) {
   return(all.dataset.entries)
   
 }
-
-
-#' Load dataset from all files included in the input.dir path
-#' 
-#' @description Read all files contained into <input.dir>. Each file is a CSV file. The file format is:
-#'
-#'   Column01: dt.year		    Dataset year.
-#'   Column02: dt.mode		    Dataset purpose mode:  training, evaluation.
-#'   Column03: dt.week		    Dataset week.
-#'   Column04: dt.day		      Dataset day of the week.
-#'   Column05: dt.type		    Dataset capture type (inside, outside, etc.).
-#'   Column06: timestamp	    Timestamp
-#'   Column07: ip_proto	 	    Ethernet frame lenght.
-#'   Column08: eth_dst	 	    Destination MAC address Hi.
-#'   Column09: eth_type	 	    Destination MAC address Low.
-#'   Column10: eth_len	 	    Source MAC address Low.
-#'   Column11: eth_src	 	    Source MAC address Hi.
-#'   Column12: icmp_checksum	Ethernet type.
-#'   Column13: icmp_code	    IP Header length.
-#'   Column14: icmp_type  	  Type of service.
-#'   Column15: ip_checksum	  IP Pacakage Lenght.
-#'   Column16: ip_dst	 	      Fragment number.
-#'   Column17: ip_fragment	  Fragment offset.
-#'   Column18: ip_frag_offset	IP Time-To-Life.
-#'   Column19: ip_hdr_len	    IP Protocol.
-#'   Column20: ip_len	 	IP CRC.
-#'   Column21: ip_src	 	Source IP.
-#'   Column22: ip_tos	 	Destination IP.
-#'   Column23: ip_ttl	 	ICMP type.
-#'   Column24: tcp_ack	 	ICMP code.
-#'   Column25: tcp_checksum	CRC ICMP.
-#'   Column26: tcp_dstport	Source Port.
-#'   Column27: tcp_flags	Destination Port.
-#'   Column28: tcp_hdr_len	TCP sequence.
-#'   Column29: tcp_options	Flag ACK.
-#'   Column30: tcp_seq	 	TCP Header Length / data offset
-#'   Column31: tcp_srcport	Flags.
-#'   Column32: tcp_urgent_pointer	Window Size.
-#'   Column33: tcp_window_size	TCP CRC.
-#'   Column34: udp_checksum	Urgent Pointer.
-#'   Column35: udp_dstport	TCP Options.
-#'   Column36: udp_length	  UDP source Port.
-#'   Column37: udp_srcport	UDP destination Port.
-#'   Column38: udp.length	  UDP length.
-#'   Column39: udp.chksum	  UDP CRC.
-#
-#' @example: loadDataset input/file...
-#' @param input.dir Path containing dataset input files
-#' @deprecated This is the first version based on test using tshark to extract informacion from tcdump files. Use "loadDataset" function instead
-loadDatasetV0 <- function(input.dir) {
-  
-  for (file in list.files(input.dir)) {
-    
-    dataset.entries <- read.csv(paste(input.dir,file,sep="/"),header=FALSE,sep=",",stringsAsFactors = FALSE)
-    
-    if (exists('all.dataset.entries')) {
-      # all.dataset.entries <- dplyr::union(all.dataset.entries,dataset.entries)
-      all.dataset.entries <- dplyr::bind_rows(all.dataset.entries,dataset.entries)
-     } else {
-      all.dataset.entries <- dataset.entries
-    }
-    break
-  }
-  
-  names(all.dataset.entries) <- c("dt.year","dt.mode","dt.week","dt.day","dt.type","ip.proto","eth.dst","eth.type","eth.len","eth.src","icmp.checksum","icmp.code","icmp.type","ip.checksum","ip.dst","ip.fragment","ip.frag_offset","ip.hdr_len","ip.len","ip.src","ip.tos","ip.ttl","tcp.ack","tcp.checksum","tcp.dstport","tcp.flags","tcp.hdr_len","tcp.options","tcp.seq","tcp.srcport","tcp.urgent_pointer","tcp.window_size","udp.checksum","udp.dstport","udp.length","udp.srcport") 
-  
-  return(all.dataset.entries)
-  
-}
-
-#' Load relation between their name and their type or category
-#' 
-#' @description Load relation between their name and their type or category. It is only one file, a CSV file with "|" separator. The file format is:
-#'
-#'   Column01: name		        Name of the attack
-#'   Column02: type   	      Category of the attack
-#'
-#' @param cache     Do not do any calculation, use precalculated data
-#' @param root.dir  Workspace root directory used to load cached data
-loadLabelAttackTypes <- function(cache = FALSE, root.dir=getwd()) {
-  
-  if( cache ) {
-    
-    label.attacktypes.raw.ds <- loadCacheLabelAttackTypesRaw(root.dir) 
-    
-  } else {
-    
-    #  Path containing dataset input files
-    input.dir <- getRawDir(root.dir)
-    
-    for (file in list.files(input.dir, pattern = PATTERN_ATTACK_TYPE_LIST)) {
-      
-      label.attacktypes.raw.ds <- read.csv(paste(input.dir,file,sep="/"),header=FALSE,sep="|",stringsAsFactors = FALSE,
-                                          colClasses=c("character", "character")
-      )
-      
-    }
-    
-    if( ! exists("label.attacktypes.raw.ds") ) {
-      print(paste("WARNING: No label attack type files found, creating an empty vector (input directory[",input.dir,"]"))
-      label.attacklist.raw.ds <- c("0","0")
-    }
-    
-    names(label.attacktypes.raw.ds) <- c("name","type");
-    
-  }  
-  
-  return(label.attacktypes.raw.ds)
-  
-}
-
-
-
-## EXTRA: Load index
-loadIndex <- function() {
-  
-  file <- '/home/nuctools/workspace/gitrepos/repos/ml-ids/index_ts_neto.txt'
-  index.ds <- read.csv(file,header=FALSE,sep=",",stringsAsFactors = FALSE,colClasses=c("double"))
-  names(index.ds) <- c('timestamp')
-  return(index.ds)
-}
-
-sortTraining <- function(index.ds, training.raw.ds) {
-  
-  require(gdata)
-  index <- as.vector(index.ds['timestamp'])
-  
-  #training.raw.ds[match(index, training.raw.ds$timestamp,]
-  
-  training.raw.ds$timestamp <- reorder.factor(training.raw.ds$timestamp, new.order=index)
-  require(dplyr)
-  training.raw.ds.sorted <- training.raw.ds %>% arrange(timestamp)
-  
-  
-  
-}
-
